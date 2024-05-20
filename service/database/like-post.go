13cdc16b5439f5425f2913c13ccc017ca91f65f0
edmii <micci.edoardo@gmail.com>
2024-05-20 20:28:04 +0200

    fix

--- service/database/like-post.go
+++ service/database/like-post.go
@@ -3,7 +3,7 @@ package database
 import "fmt"
 
 func (db *appdbimpl) LikePost(PostID int, OwnerID int) error {
-	query := `INSERT INTO LikesDB (LikedPhotoID, Owner_id) VALUES ($1, $2)`
+	query := `INSERT INTO LikesDB (LikedPhotoID, Ownerid) VALUES ($1, $2)`
 
 	_, err := db.c.Exec(query, PostID, OwnerID)
 	if err != nil {
