4381c87a01c0d052acd3f2be59636565086bd243
edmii <micci.edoardo@gmail.com>
2024-05-23 15:11:40 +0200

    comment in request body

--- service/api/comment_post.go
+++ service/api/comment_post.go
@@ -1,12 +1,17 @@
 package api
 
 import (
+	"encoding/json"
 	"net/http"
 	"strconv"
 
 	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/api/reqcontext"
 	"github.com/julienschmidt/httprouter"
 )
+
+type Comment struct {
+	Content string `json:"content"`
+}
 
 func (rt *_router) CommentPost(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
 	if r.Method != "POST" {
@@ -41,14 +46,21 @@ func (rt *_router) CommentPost(w http.ResponseWriter, r *http.Request, ps httpro
 		return
 	}
 
-	content := ps.ByName("content")
+	var comment Comment
 
-	if content == "" {
+	err = json.NewDecoder(r.Body).Decode(&comment)
+	if err != nil {
+		ctx.Logger.Info("Failed to decode request body", err.Error())
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	if comment.Content == "" {
 		http.Error(w, "missing content", http.StatusBadRequest)
 		return
 	}
 
-	err = rt.db.CommentPost(postID, ownerID, content)
+	err = rt.db.CommentPost(postID, ownerID, comment.Content)
 	if err != nil {
 		ctx.Logger.Info("Failed to comment post", err.Error())
 		http.Error(w, err.Error(), http.StatusInternalServerError)
