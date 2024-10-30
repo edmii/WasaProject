75064f9f693806847a95151d6d3b817a2c81b7af
edmii <micci.edoardo@gmail.com>
2024-10-30 16:15:30 +0100

    get banned users

--- service/database/ban-user.go
+++ service/database/ban-user.go
@@ -11,8 +11,20 @@ type Banned struct {
 
 func (db *appdbimpl) BanUser(OwnerID int, PrayID int) (int, error) {
 	var exists bool
+
+	// check if owner and pray exist in db
+	checkQuery1 := `SELECT EXISTS(SELECT 1 FROM UserDB WHERE UserID = $1) AND EXISTS(SELECT 1 FROM UserDB WHERE UserID = $2)`
+	err := db.c.QueryRow(checkQuery1, OwnerID, PrayID).Scan(&exists)
+	if err != nil {
+		return 0, fmt.Errorf("failed to check user existence: %w", err)
+	}
+
+	if !exists {
+		return 0, fmt.Errorf("user does not exist")
+	}
+
 	checkQuery := `SELECT EXISTS(SELECT 1 FROM BanDB WHERE OwnerID = $1 AND PrayID = $2)`
-	err := db.c.QueryRow(checkQuery, OwnerID, PrayID).Scan(&exists)
+	err = db.c.QueryRow(checkQuery, OwnerID, PrayID).Scan(&exists)
 	if err != nil {
 		return 0, fmt.Errorf("failed to check ban existence: %w", err)
 	}
