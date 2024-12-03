74ea95cadef523569ff0ca387c7d10e51459504f
edmii <micci.edoardo@gmail.com>
2024-12-03 13:16:10 +0100

    delete likes and comments after ban

--- service/database/ban-user.go
+++ service/database/ban-user.go
@@ -46,12 +46,64 @@ func (db *appdbimpl) BanUser(OwnerID int, PrayID int) (int, error) {
 		}
 		return 1, nil
 	} else {
+
+		// Begin a transaction
+		tx, err := db.c.Begin()
+		if err != nil {
+			return 0, fmt.Errorf("failed to begin transaction: %w", err)
+		}
+
+		// Defer rollback to ensure any error rolls back transaction
+		defer func() {
+			if err != nil {
+				tx.Rollback()
+			}
+		}()
+
 		// If the ban does not exist, insert it
 		insertQuery := `INSERT INTO BanDB (OwnerID, PrayID) VALUES ($1, $2)`
-		_, err := db.c.Exec(insertQuery, OwnerID, PrayID)
+		_, err = tx.Exec(insertQuery, OwnerID, PrayID)
 		if err != nil {
 			return 0, fmt.Errorf("failed to insert ban: %w", err)
 		}
+
+		// Query to delete likes related to the post
+		deleteFollowQuery := `
+		DELETE FROM FollowDB 
+		WHERE (OwnerID = $1 AND FollowedID = $2) 
+		OR (OwnerID = $2 AND FollowedID = $1)`
+
+		_, err = tx.Exec(deleteFollowQuery, OwnerID, PrayID)
+		if err != nil {
+			return 0, fmt.Errorf("failed to delete follow: %w", err)
+		}
+
+		deleteLikesQuery := `
+        DELETE FROM LikesDB 
+		WHERE (OwnerID = $1 AND LikedPhotoID IN (SELECT PostID FROM PostDB WHERE OwnerID = $2)) 
+		OR (OwnerID = $2 AND LikedPhotoID IN (SELECT PostID FROM PostDB WHERE OwnerID = $1));
+    `
+		_, err = tx.Exec(deleteLikesQuery, OwnerID, PrayID)
+		if err != nil {
+			return 0, fmt.Errorf("failed to delete likes: %w", err)
+		}
+
+		// Query to delete comments
+		deleteCommentsQuery := `
+        DELETE FROM CommentsDB
+        WHERE (OwnerID = $1 AND PhotoID IN (SELECT PostID FROM PostDB WHERE OwnerID = $2))
+        OR (OwnerID = $2 AND PhotoID IN (SELECT PostID FROM PostDB WHERE OwnerID = $1));
+    `
+		_, err = tx.Exec(deleteCommentsQuery, OwnerID, PrayID)
+		if err != nil {
+			return 0, fmt.Errorf("failed to delete comments: %w", err)
+		}
+
+		// Commit the transaction if successful
+		if err = tx.Commit(); err != nil {
+			return 0, fmt.Errorf("failed to commit transaction: %w", err)
+		}
+
 		return 2, nil
 	}
 
