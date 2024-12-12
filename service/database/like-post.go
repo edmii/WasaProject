525ffb8e738a5588f53d00d0ace165a94c568864
edmii <micci.edoardo@gmail.com>
2024-12-12 16:56:43 +0100

    login

--- service/database/like-post.go
+++ service/database/like-post.go
@@ -5,7 +5,7 @@ import "fmt"
 func (db *appdbimpl) LikePost(PostID int, OwnerID int) (int, error) {
 	var user2ID int
 
-	userQuery := "SELECT OwnerID from PostDB where PostID $1"
+	userQuery := "SELECT OwnerID from PostDB where PostID = $1"
 	err := db.c.QueryRow(userQuery, PostID).Scan(&user2ID)
 	if err != nil {
 		return 0, fmt.Errorf("failed to retrieve user posts: %w", err)
